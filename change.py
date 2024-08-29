6ced749e83651d0ec4090267b131fbac371f770d
Amruthaprasanth <amruthaprasanth00@gmail.com>
2024-08-29 09:13:56 +0530

    Add files via upload

---
+++ change.py
@@ -0,0 +1,158 @@
+# languages = ['Python', 'Swift', 'C++']
+# languages[2] = 'C'
+# print(languages) 
+# prime_numbers=[2,3,5,7]
+# removed_element=prime_numbers.pop(2)
+# print('removed element:',removed_element)
+# print('updated list:',prime_numbers)
+# languages=['python','swift','c++','c','java','rust','R']
+
+# del languages[1]
+# print(languages)
+# del languages[-1]
+# print(languages)
+# del languages[0:2]
+# print(languages)
+
+# languages = ['Python','Swift','C++','C','Java','Rust','R']
+# languages.remove('Python')
+# print(languages) # ['Swift','C++','C','Java','Rust','R’]
+
+# prime_numbers=[2,3,5,7]
+# prime_numbers.reverse()
+# print('reversed list:',prime_numbers)
+
+# list1=[12,14,16,18,20]
+# l=list1*2
+# print(l)
+
+# list1=[12,14,16,18,20]
+# list2=[9,10,32,54,86]
+# l=list1+list2
+# print('concatenated list',l)
+
+# list1=[12,14,16,18,20,23,27,39,40]
+# a=len(list1)
+# print('length of the list',a)
+
+# list1=[12,14,16,39,40]
+# for i in list1:
+#     print(i)
+
+# list1=[100,200,300,400,500]
+# print(600 in list1)
+# print(700 in list1)
+
+# list1=[103,675,321,782,200]
+# print(max(list1))
+
+# list1=[103,675,321,782,200]
+# print(min(list1))
+
+# list1 = [1, 2, 3, 4, 5]
+# list2 = [3, 4, 5, 6, 7]
+# intersection1 = set(list1).intersection(list2)
+# print(intersection1)
+
+# intersection2 = set(list1) & set(list2)
+# print(intersection2) 
+
+# a=set()  # empty set
+# print(type(a))
+
+# set1={1,6,4,'abc'}
+# print(type(set1))
+
+# Days=set(["Monday","Tuesday","Wednesday","Thursday"])
+# print(Days)
+# print(type(Days))
+# for i in Days:
+#     print(i)
+
+# Months=set(["January","February","March","April", "May","June"])
+# print(Months)
+# Months.add("July")
+
+# Months=set(["January","February","March","April", "May","June"])
+# print(Months)
+# Months.update(["July","August"])
+# print(Months)
+
+# months=set(["January","February","March"]) 
+# print(months)
+# months.discard("January")
+# print(months)
+
+# months=set(["January","February","March","April"])      
+# print(months)
+# months.remove("January")
+# print(months)
+
+# Days1={"Monday","Tuesday","Wednesday","Thursday"}
+# Days2={"Friday","Saturday","Sunday"}
+# print(Days1|Days2)  
+
+# Days1={"Monday","Tuesday","Wednesday","Thursday"}
+# Days2={"Monday","Tuesday","Sunday","Friday"}
+# print(Days1&Days2)
+
+# Days1={"Monday","Tuesday","Wednesday","Thursday"}
+# Days2={"Monday","Tuesday"}
+# Days3={"Monday","Tuesday","Friday"}
+# print(Days1>Days2)
+# print(Days1<Days2)
+# print(Days2==Days3)
+
+# Tuple_data=(0,1,2,3,2,3,1,3,2)
+# Print(Tuple_data)
+
+# letters =("p", "r", "o", "g", "r", "a", "m", "i", "z")
+# print(letters[0])
+# print(letters[5])
+
+# letters = ('p', 'r', 'o', 'g', 'r', 'a', 'm', 'i', 'z')
+# print(letters[-1]) 
+# print(letters[-3])
+
+# tuple_=('Python',"Tuples")
+# print("Original tuple is:",tuple_)
+# tuple_=tuple_*3
+# print("New tuple is:",tuple_)
+
+# languages = ('Python', 'Swift', 'C++')
+# print(C in languages)
+# print(Python in languages)
+
+
+# new_tuple = ('a', 'b', 'c', 'd')
+# my_list = list(new_tuple)
+# my_list[1] = 'z'
+# new_tuple = tuple(my_list)
+# print(new_tuple)
+
+# set1 ={1,2,3,4}
+# set2 ={3,4,5,6}
+# union_set = set1.union(set2)
+# intersection_set = set1.intersection(set2)
+# print("Union:", union_set)
+# print("Intersection:", intersection_set)
+
+# person = {'name': 'John','age': 25,'city': 'New York'}
+# person['age']=30
+# person['job']='Engineer'
+# print(person)
+
+# list1 = [1, 2, 3, 4, 5]
+# new_tuple = tuple(list1)
+# print(new_tuple)
+
+# d1={'x':10,'y':20}
+# d2={'z':30}
+# d1.update(d2)
+# print(d1)
+
+
+
+
+
+
